5084c1d4d9b002b78130c4eb0e337e7ded278997
Subrat1312 <subratsahu405@gmail.com>
2022-11-29 20:10:30 +0530

    'first'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import ComponentA from './ComponentA';
+
+function App() {
+  return (
+    <div className="App">
+      <ComponentA />
+    </div>
+  );
+}
+
+export default App;
